e1421d4c7465d8be6a0713c49fa7c609810aa104
Ishtiaq uddin <ishty119@gmail.com>
2022-05-12 19:11:41 +0600

    navber and route set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointmet/Appointment";
+import ContactUs from "./Pages/Contact Us/ContactUs";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Reviews from "./Pages/Reviews/Reviews";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="Appointment" element={<Appointment />}></Route>
+        <Route path="reviews" element={<Reviews />}></Route>
+        <Route path="contact" element={<ContactUs />}></Route>
+      </Routes>
     </div>
   );
 }
