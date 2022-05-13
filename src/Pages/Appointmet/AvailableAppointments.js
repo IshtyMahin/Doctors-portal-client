dcfe7f50eac8d00306e573d3f812190d5a07470c
Ishtiaq uddin <ishty119@gmail.com>
2022-05-13 19:19:36 +0600

    add with serrver site , mongodb

--- src/Pages/Appointmet/AvailableAppointments.js
+++ src/Pages/Appointmet/AvailableAppointments.js
@@ -6,7 +6,7 @@ const AvailableAppointments = ({ date }) => {
   const [services,setServices] = useState([]);
   const [treatment,setTreatment]=useState(null)
   useEffect(()=>{
-    fetch('services.json')
+    fetch('http://localhost:5000/service')
     .then(res=> res.json())
     .then(data => setServices(data))
   },[])
